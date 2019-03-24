21ab29ccd5eb488c3cfc0f214da0b6073e019d73
Kade Walter <kadwalter1@gmail.com>
2019-03-24 14:16:25 -0500

    c++ imp on mac

--- cppMac/LinkedList.cpp
+++ cppMac/LinkedList.cpp
@@ -5,18 +5,18 @@ using namespace std;
 struct Node
 {
 	int value;
-	Node* next;
+	Node *next;
 };
 
-Node* newNode(int value)
+Node *newNode(int value)
 {
-	Node* temp = new Node;
+	Node *temp = new Node;
 	temp->value = value;
 	temp->next = NULL;
 	return temp;
 }
 
-void PrintList(Node* List)
+void PrintList(Node *List)
 {
 	Node *current = List;
 	while (current != NULL)
@@ -24,28 +24,87 @@ void PrintList(Node* List)
 		cout << current->value << endl;
 		current = current->next;
 	}
-
 }
 
-void Insert(Node* &List, int value)
+void InsertAtTail(Node *&List, int value)
 {
-    if(List == NULL)
-        List = newNode(value);
-    else 
-    {
-        Node* curr = List;
-        while(curr->next != NULL)
-            curr = curr->next;
-        curr->next = newNode(value);
-    }
+	if (List == NULL)
+		List = newNode(value);
+	else
+	{
+		Node *curr = List;
+		while (curr->next != NULL)
+			curr = curr->next;
+		curr->next = newNode(value);
+	}
+}
+
+void InsertAtHead(Node *&List, int value)
+{
+	if (List == NULL)
+		List = newNode(value);
+	else
+	{
+		Node *nodeToAdd = newNode(value);
+		nodeToAdd->next = List;
+		List = nodeToAdd;
+	}
+}
+
+void InsertInOrder(Node *&List, int value)
+{
+	if (List == NULL)
+		List = newNode(value);
+	else
+	{
+		Node *curr = List;
+		if (curr->value > value) // check if we need to insert at the start
+		{
+			InsertAtHead(List, value);
+		}
+		else
+		{
+			while (curr->next != NULL && curr->next->value < value)
+				curr = curr->next;
+			if (curr->next != NULL) // used for inserting in the middle
+			{
+				Node *temp = newNode(value);
+				temp->next = curr->next;
+				curr->next = temp;
+			}
+			else
+			{
+				InsertAtTail(List, value); // used to insert at the end
+			}
+		}
+	}
 }
 
 int main()
 {
-	Node* LinkedList = NULL;
+	Node *LinkedList = NULL;
+
+	/* uncomment to test add at tail.
+	// Insert always at the tail.
 	for (int i = 0; i <= 100; i++)
-		Insert(LinkedList, i);
+		InsertAtTail(LinkedList, i);
+	*/
+
+	/* uncomment to test add at tail.
+	// Insert always at the head.
+	for(int i = 100; i >= 0; i--)
+		InsertAtHead(LinkedList, i);
+	*/
+
+	// Insert in order.
+	for (int i = 100; i >= 0; i--)
+	{
+		int randomNumToInsert;
+		randomNumToInsert = rand() % 100 + 1;
+		InsertInOrder(LinkedList, randomNumToInsert);
+	}
 	PrintList(LinkedList);
 	cout << "Press Enter to Continue." << endl;
-	cin.ignore(); cin.ignore();
+	cin.ignore();
+	cin.ignore();
 }
